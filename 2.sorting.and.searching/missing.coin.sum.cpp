0fafa55ba9ff57a8e54370e07f84ce283d6e5037
Konstantin Saveljev <konstantin.saveljev@bolt.eu>
2021-04-09 21:17:27 +0300

    Missing Coin Sum solved

---
+++ 2.sorting.and.searching/missing.coin.sum.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <iomanip>
+#include <cmath>
+#include <cstring>
+#include <numeric>
+#include <cstdio>
+#include <list>
+#include <cassert>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define loop(a) for(int i = 0; i < a; i++)
+#define loopv(i,a) for (int i = 0; i < a; i++)
+#define rep(i,a,b) for (int i = a; i < b; i++)
+#define all(x) (x).begin(), (x).end()
+#define prDouble(x) cout << fixed << setprecision(10) << x
+#define goog(tno) cout << "Case #" << tno <<": "
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+int main(void) {
+    int n;
+    cin >> n;
+
+    vector<ll> nums(n);
+
+    for (auto &x : nums) {
+        cin >> x;
+    }
+
+    sort(nums.begin(), nums.end());
+
+    ll result = 1;
+    int i = 0;
+
+    while (i < n && nums[i] <= result) {
+        result += nums[i];
+        i++;
+    }
+
+    cout << result << endl;
+
+    return 0;
+}
